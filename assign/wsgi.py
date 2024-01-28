0eb736803a1e1aa18218318d3982b05163d0d1be
Shradha Arishenapalli <its.shivani.0925@gmail.com>
2024-01-29 00:46:44 +0530

    added wsgi file for python anywhere

--- assign/wsgi.py
+++ assign/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assign.settings')
+
+
+
+# add your project directory to the sys.path
+project_home = '/home/Shivani0925/Invoice'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'assign.settings'
+
+
+# serve django via WSGI
 
 application = get_wsgi_application()
