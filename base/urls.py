1c0f1840335056204221b29d120a14a44d2ad027
Shradha Arishenapalli <its.shivani.0925@gmail.com>
2024-01-29 01:20:42 +0530

    allowing python anywhere host

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.front),  
+    #path('', views.front),  
     path('invoices/', views.all_invoices,name='all-invoices'),
     path('invoices/<int:invoice_id>/', views.details, name='invoice-detail'),
 ]
